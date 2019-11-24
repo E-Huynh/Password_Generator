9eb51c27d08ee3f98819b8ac2a15a6a0ea71c650
Erik Huynh <Erik.W.Huynh@gmail.com>
2019-11-24 10:01:38 -0700

    Initial start of html/css

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,3 +1,7 @@
+        //TODO
+        //incorporate 8-128 character limit
+        //tie in user inputs to something other than prompts/confirms
+        
         var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
         var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
         var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
